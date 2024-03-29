a19dca1d58c916eda394dde6589d7c27b209ef83
khadem-mh <khadem13359@gmail.com>
2024-03-29 20:12:24 +0330

    init project and create files important and develops page

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const subTitleElem = document.querySelector('.subtitle')
+const titleElem = document.querySelector('.title')
+
+const colors = [
+    'red',
+    'blue',
+    'green',
+    'skyblue',
+    'gold',
+    'gray',
+    'brown',
+    'white',
+    'black'
+]
+
+window.addEventListener('click', () => {
+    let indexChosseForColor = Math.floor(Math.random() * colors.length)
+    if (colors[indexChosseForColor] == 'black') {
+        subTitleElem.classList.add('color-font')
+        titleElem.classList.add('color-font')
+    }
+    document.documentElement.style.backgroundColor = colors[indexChosseForColor]
+    subTitleElem.innerHTML = colors[indexChosseForColor]
+})
